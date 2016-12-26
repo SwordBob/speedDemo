3cfff2520779b15d2742783e6f2a770cbd5fd792
SwordBob <774327136@qq.com>
2016-12-26 18:10:02 +0800

    补全遗漏

---
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -0,0 +1,16 @@
+package org.seckill.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.seckill.entity.Seckill;
+
+public interface SeckillDao {
+
+	//1.减库存，
+	int reduceNumber(long seckillId,Date killTime);
+	//2. 根据id查询秒杀对象 
+	Seckill queryById(long seckillId);
+	//3. 根据偏移量查询秒杀商品列表：
+	List<Seckill> queryALL(int offset,int limit);
+}
